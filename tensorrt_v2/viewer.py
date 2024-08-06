6c6f30016c3580a5756b227656c60da8518be41f
hugo.liu <hugo.liu@siengine.com>
2024-08-06 19:14:03 +0800

    add tensorrt_v2, using CMakeLists.txt instead of Makefile
    Signed-off-by: hugo.liu <hugo.liu@siengine.com>

---
+++ tensorrt_v2/viewer.py
@@ -0,0 +1,35 @@
+"""
+author: hova88
+date: 2021/03/16
+"""
+import numpy as np 
+import numpy as np
+from visual_tools import draw_clouds_with_boxes
+import open3d as o3d
+import yaml
+
+# def cfg_from_yaml_file(cfg_file, config):
+#     with open(cfg_file, 'r') as f:
+#         try:
+#             new_config = yaml.load(f, Loader=yaml.FullLoader)
+#         except:
+#             new_config = yaml.load(f)
+#         merge_new_config(config=config, new_config=new_config)
+#     return config
+
+def dataloader(cloud_path , boxes_path):
+    ### x, y, z, intensity, time_lag
+    cloud = np.loadtxt(cloud_path).reshape(-1,5)
+    #/** use pointpillar box [ x, y, z, dx, dy, dz, yaw, score, cls] */
+    boxes = np.loadtxt(boxes_path).reshape(-1,9) #7
+    return cloud , boxes 
+
+if __name__ == "__main__":
+    import yaml
+    # with open("../bootstrap.yaml") as f:
+    #     config = yaml.load(f, Loader=yaml.FullLoader)
+    # input_pcd_file = "/home/hugoliu/github/nvidia/lidar/CenterPoint/tensorrt/data/centerpoint/points/0a0d6b8c2e884134a3b48df43d54c36a.bin"
+    input_pcd_file = "/home/hugoliu/github/nvidia/lidar/CenterPoint/tensorrt/data/centerpoint/points/nuscenes_10sweeps_points.txt"
+    output_box_file = "/home/hugoliu/github/nvidia/lidar/CenterPoint/tensorrt/data/centerpoint/fp32_5.txt"
+    cloud ,boxes = dataloader(input_pcd_file, output_box_file)
+    draw_clouds_with_boxes(cloud ,boxes)
